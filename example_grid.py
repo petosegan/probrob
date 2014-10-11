fea96154748bee5b8a184862d89c9a60274e5874
Richard Turner <rwturner@stanford.edu>
2014-10-11 10:35:31 -0700

    Corrected Example Grid

--- example_grid.py
+++ example_grid.py
@@ -1,26 +1,29 @@
 ''' http://scriptogr.am/jdp/post/pathfinding-with-python-graphs-and-a-star '''
 
-from astar_grid import AStarGrid, AStarGridNode
+from astargrid import AStarGrid, AStarGridNode
 from itertools import product
 
 def make_graph(mapinfo):
-    nodes = [[AStarGridNode(x, y) for y in range(mapinfo.height)] 
-                            for x in range(mapinfo.width)]
+    nodes = [[AStarGridNode(x, y) for y in range(mapinfo['height'])] 
+                            for x in range(mapinfo['width'])]
     graph = {}
-    for x, y in product(range(mapinfo.width), range(mapinfo.height)):
+    for x, y in product(range(mapinfo['width']), range(mapinfo['height'])):
         node = nodes[x][y]
         graph[node] = []
         for i, j in product([-1, 0, 1], [-1, 0, 1]):
-            if not (0 <= x + i < mapinfo.width): continue
-            if not (0 <= y + j < mapinfo.height): continue
+            if not (0 <= x + i < mapinfo['width']): continue
+            if not (0 <= y + j < mapinfo['height']): continue
             graph[nodes[x][y]].append(nodes[x+i][y+j])
     return graph, nodes
 
-graph, nodes = make_graph({"width": 8, "height": 8})
-paths = AStarGrid(graph)
-start, end = nodes[1][1], nodes[5][7]
-path = paths.search(start, end)
-if path is None:
-    print "No path found"
-else:
-    print "Path found:", path
+    
+if __name__ == "__main__":
+    graph, nodes = make_graph({"width": 8, "height": 8})
+    paths = AStarGrid(graph)
+    start, end = nodes[1][1], nodes[5][7]
+    path = paths.search(start, end)
+    path = [(node.x, node.y) for node in path]
+    if path is None:
+        print "No path found"
+    else:
+        print "Path found:", path
