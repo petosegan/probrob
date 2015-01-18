33d886d46fce457a05b1c675e40316250b24f3e8
Richard Turner <rwturner@stanford.edu>
2015-01-18 15:29:13 -0800

    change file structure for packaging

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup, Command
+from distutils.extension import Extension
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys, subprocess
+
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
+setup(
+    name='probrob',
+    version='',
+    packages=['probrob'],
+    url='',
+    license='',
+    author='Richard W. Turner',
+    author_email='rwturner@stanford.edu',
+    description='Probabilistic Robotics Simulator',
+    requires=['numpy', 'matplotlib', 'scipy'],
+    cmdclass={'test', PyTest},
+    ext_modules = [Extension("ray_trace", ["ray_trace.c"])]
+)
